42b512bc270c3fa8e8df34a951780bf77b3e29a3
Allen <rcvr1024@gmail.com>
2022-07-30 11:44:22 +0800

    add sum.js hello.js

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log('hello nodejs');
+
+// window是瀏覽器物件 nodejs無法使用
+// window.location.href = "http://www.google.com";
---
+++ sum.js
@@ -0,0 +1,12 @@
+
+function sum(n){
+    let s = 0;
+    for(i=1; i<=n;i++){
+        s+=i;
+    }
+    return s;
+}
+
+console.log(sum(1));
+console.log(sum(3));
+console.log(sum(10));
